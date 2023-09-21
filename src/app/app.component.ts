e954d60f302802eb11bba516858ddf480b30e2c4
Benji Dalton <benjamin.dalton@outlook.com>
2023-09-21 11:20:36 -0400

    On branch master Changes to be committed: 	modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'physical-activity-tracker';
+	title = 'physical-activity-tracker';
+
+	// new run button that pops ups a window when clicked
+	// field entries for time, distance, temperature, humidity, elevation
+	// submit button that sends info to python script that stores inputted information into SQL database
+
 }
